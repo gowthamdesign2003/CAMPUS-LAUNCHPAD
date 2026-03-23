ab9d59acba7bebf386514119a8b8c1c029509295
Karthik-Velt <kathik@velt.dev>
2026-03-23 22:40:19 +0530

    Updated status

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -134,7 +134,7 @@ const Dashboard = () => {
             {/* Header Flex */}
             <div className="flex flex-col md:flex-row md:items-center justify-between gap-6 mb-10">
                 <div>
-                    <h1 className="text-2xl font-bold text-gray-900">Provide Opportunities</h1>
+                    <h1 className="text-2xl font-bold text-gray-900">Provide Opportunitie</h1>
                     <p className="text-gray-500 text-sm mt-1">Manage job postings and student applications.</p>
                 </div>
                 
